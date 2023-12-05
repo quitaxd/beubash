a79a43595a04fadea381cefc37688048b7ca02ae
Quitaxd <vkramazan2@gmail.com>
2023-12-05 21:12:24 +0300

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if command -v lolcat >/dev/null; then
+	echo "Starting the installer."
+	sleep 0.5
+else
+	echo "Lolcat is not installed. Please install it."
+	exit 1
+fi
+
+echo "##############################" | lolcat
+echo "### Start the installation ###" | lolcat
+echo "##############################" | lolcat
+sleep 0.5
+
+clear
+
+echo "Cloning the repo." | lolcat
+git clone https://github.com/Quitaxd/beubash ~/.beubash
+
+if ls ~/.bashrc >/dev/null; then
+	echo "" >> ~/.bashrc
+	echo "~/.beubash/scripts/*.sh" >> ~/.bashrc
+	echo "source ~/.beubash/aliases.conf" >> ~/.bashrc
+else
+	touch ~/.bashrc
+	echo "" >> ~/.bashrc
+	echo "~/.beubash/scripts/*.sh" >> ~/.bashrc
+        echo "source ~/.beubash/aliases.conf" >> ~/.bashrc
+fi
