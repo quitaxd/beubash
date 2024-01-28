6842ef95c34755000a8706220ebbad2554a5febd
Quitaxd <108627333+Quitaxd@users.noreply.github.com>
2024-01-28 22:27:49 +0300

    Update install.dev.sh

--- install.dev.sh
+++ install.dev.sh
@@ -49,3 +49,4 @@ clear
 
 echo "NOTE: You can put your aliases into $HOME/.config/aliasrc"
 source ~/.bashrc
+echo "If beubash was not activated, please close this terminal and open a new terminal."
